cb956afbaa063f9c9055a6442f9487077f44bacb
woozooz <ajk0830@gmail.com>
2026-05-11 18:18:09 +0900

    chore(fw): main.c 코드 정리

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -32,6 +32,9 @@
 #include "sensor_types.h"
 #include "bme280.h"
 #include "imu.h"
+#include "blt.h"
+#include "mode.h"
+#include "motor.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -52,17 +55,11 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-uint8_t bl_data;          // 방금 막 들어온 따끈따끈한 데이터 1개
-uint8_t bl_buffer[6];     // 6개가 다 찰 때까지 모아둘 바구니
-int bl_index = 0;
-
-
 BME280_Data bme_data = {0};
 IMU_Data imu_data = {0};
 SensorPacket packet = {0};
 
 char msg[128];
-
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -111,15 +108,12 @@ int main(void)
   MX_USART1_UART_Init();
   MX_UART4_Init();
   MX_CRC_Init();
+
   /* USER CODE BEGIN 2 */
   BME280_Init();
   IMU_Init();
-
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1); // 왼쪽 모터 속도 엔진 START
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2); // 오른쪽 모터 속도 엔진 START
-
-  // 블루투스 신호 대기 (1바이트씩 인터럽트 방식으로 받기)
-  HAL_UART_Receive_IT(&huart4, &bl_data, 1);
+  Motor_Init();
+  BLT_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -134,6 +128,8 @@ int main(void)
 
 	  packet.bme_data = bme_data;
 	  packet.imu_data = imu_data;
+
+	  Process_By_Mode();
 
 	  sprintf(msg, "온도:%f roll:%f pitch:%f yaw:%f\r\n",
 	          packet.bme_data.temperature,
@@ -196,49 +192,12 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-void Move_Robot(uint8_t left_dir, uint8_t left_speed, uint8_t right_dir, uint8_t right_speed) {
-    // 왼쪽 모터 방향 (1: 전진, 0: 후진)
-    if(left_dir == 1) {
-    	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);   // PC0을 High로
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET); // PC1을 Low로
-    } else {
-    	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET);
-    }
-
-    if(right_dir == 1) {
-		// 전진 신호(1)가 왔을 때, 반대로 돌게 함
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET); // SET -> RESET으로 수정
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_SET);   // RESET -> SET으로 수정
-	} else {
-		// 후진 신호(0)가 왔을 때
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);   // RESET -> SET으로 수정
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_RESET); // SET -> RESET으로 수정
-	}
-
-    // 속도 제어 (ARR이 255이므로 받은 데이터 그대로 사용)
-    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, left_speed);
-    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, right_speed);
-}
-
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 	if (huart->Instance == USART3) {
 		IMU_RxCallback();
-} else if (huart->Instance == UART4) {
-	bl_buffer[bl_index++] = bl_data; // 바구니에 담고 번호 +1
-
-		if(bl_index >= 6) { // 6개가 다 모였다면?
-			// 패킷 검사 (시작: 0xAA, 끝: 0x55)
-			if(bl_buffer[0] == 0xAA && bl_buffer[5] == 0x55) {
-				// 데이터 순서: [0]AA [1]L방향 [2]L속도 [3]R방향 [4]R속도 [5]55
-				Move_Robot(bl_buffer[1], bl_buffer[2], bl_buffer[3], bl_buffer[4]);
-			}
-			bl_index = 0; // 바구니 비우기 (초기화)
-		}
-
-		// 중요: 다음 데이터를 받기 위해 다시 수신 대기 상태로 만듦
-		HAL_UART_Receive_IT(&huart4, &bl_data, 1);
-}
+} 	else if (huart->Instance == UART4) {
+		BLT_ProcessPacket();
+	}
 }
 /* USER CODE END 4 */
 
