9e631899b3eab93e61f8a8854c25d3cab8ba4db2
김지우 <rlawldn0412@gmail.com>
2026-05-13 15:00:41 +0900

    feat(fw): BME280 폴링 → RPi 요청 트리거 방식으로 전환

--- firmware/Core/Src/bme280.c
+++ firmware/Core/Src/bme280.c
@@ -7,6 +7,11 @@ static int ma_index = 0;
 static float lp_filtered = 0.0f;
 static float alpha = 0.1f;
 
+
+
+static volatile uint8_t bme_request_flag = 0;
+volatile uint8_t rpi_rx_buf;
+
 // 데이터 보정값
 uint16_t dig_T1;
 int16_t  dig_T2, dig_T3;
@@ -23,21 +28,23 @@ void BME280_ReadCalibration(void) {
 /*
  * BME280 센서에서 온도 raw값을 읽어서 필터링 적용
  */
-void BME280_ReadData(BME280_Data *data) {
+void BME280_Process(void) {
+	if (!bme_request_flag) return;
+	bme_request_flag = 0;
+
 	uint8_t raw[3];
 	HAL_I2C_Mem_Read(&hi2c1, 0x77 << 1, 0xFA, 1, raw, 3, 100);
 
 	int32_t adc_T = ((int32_t)raw[0] << 12) | ((int32_t)raw[1] << 4) | (raw[2] >> 4);
+	      int32_t var1 = ((((adc_T >> 3) - ((int32_t)dig_T1 << 1))) * ((int32_t)dig_T2)) >> 11;
+	      int32_t var2 = (((((adc_T >> 4) - ((int32_t)dig_T1)) * ((adc_T >> 4) - ((int32_t)dig_T1))) >> 12) *
+	  ((int32_t)dig_T3)) >> 14;
+	      float temp = (float)(((var1 + var2) * 5 + 128) >> 8) / 100.0f;
 
-	int32_t var1, var2, t_fine;
-	var1 = ((((adc_T >> 3) - ((int32_t)dig_T1 << 1))) * ((int32_t)dig_T2)) >> 11;
-	var2 = (((((adc_T >> 4) - ((int32_t)dig_T1)) * ((adc_T >> 4) - ((int32_t)dig_T1))) >> 12) * ((int32_t)dig_T3)) >> 14;
-	t_fine = var1 + var2;
 
-	float raw_tmp = (float)((t_fine * 5 + 128) >> 8) / 100.0f;
-	data->bme_tick = HAL_GetTick();
-	data->temperature = raw_tmp ;
-	data->temperature_f = BME280_Filter(raw_tmp);
+	char buf[16];
+	snprintf(buf, sizeof(buf), "%.2f\n", temp);
+	HAL_UART_Transmit(&huart1, (uint8_t*)buf, strlen(buf), 100);
 }
 
 /*
@@ -57,12 +64,20 @@ float BME280_Filter(float new_temp) {
     return lp_filtered;
 }
 
+void BME280_RxCallback(void) {
+	bme_request_flag = 1;
+}
+
 
 void BME280_Init(void) {
 	// 온도 x1 Normal 모드 설정
 	uint8_t config_meas = 0x27;
 	HAL_I2C_Mem_Write(&hi2c1, 0x77 << 1, 0xF4, 1, &config_meas, 1, 100);
 
-	// 보정값 읽기
+	// 보정값
+
 	BME280_ReadCalibration();
+
+	// 수신읽기
+	HAL_UART_Receive_IT(&huart1, (uint8_t*)&rpi_rx_buf, 1);
 }
