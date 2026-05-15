241876a6275f3bc15ff765352120a9439209d66e
김지우 <rlawldn0412@gmail.com>
2026-05-15 10:42:56 +0900

    feat(fw): 라즈베리파이 UART 수신에 stop_flag 파싱 추가
    - rpi_stop_flag 변수 선언
    - sscanf 포맷에 %hhu 추가하여 stop_flag 수신 처리

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -58,9 +58,9 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-BME280_Data bme_data = {0};
-IMU_Data imu_data = {0};
-SensorPacket packet = {0};
+BME280_Data bme_data = { 0 };
+IMU_Data imu_data = { 0 };
+SensorPacket packet = { 0 };
 
 char msg[256];
 
@@ -70,10 +70,11 @@ extern uint8_t target_speed;
 extern uint8_t target_dir;
 
 // raspi 통신
-static uint8_t rpi_cmd   = 0;
+static uint8_t rpi_cmd = 0;
 static uint8_t rpi_state = 0;
 static char rpi_str_buf[32];
 static uint8_t rpi_str_idx = 0;
+uint8_t rpi_stop_flag = 0;
 
 /* USER CODE END PV */
 
@@ -88,176 +89,171 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_TIM2_Init();
-  MX_I2C1_Init();
-  MX_USART2_UART_Init();
-  MX_USART3_UART_Init();
-  MX_USART1_UART_Init();
-  MX_UART4_Init();
-  MX_CRC_Init();
-  MX_TIM4_Init();
-  /* USER CODE BEGIN 2 */
-  BME280_Init();
-  IMU_Init();
-  Motor_Init();
-  BLT_Init();
-  Nav_Init(2.0f, 0.0f, 0.0f); // PID 튜닝위해 초기값 설정 Kp=2.0
-  HAL_TIM_Base_Start_IT(&htim4);
-  /* USER CODE END 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_TIM2_Init();
+	MX_I2C1_Init();
+	MX_USART2_UART_Init();
+	MX_USART3_UART_Init();
+	MX_USART1_UART_Init();
+	MX_UART4_Init();
+	MX_CRC_Init();
+	MX_TIM4_Init();
+	/* USER CODE BEGIN 2 */
+	BME280_Init();
+	IMU_Init();
+	Motor_Init();
+	BLT_Init();
+	Nav_Init(2.0f, 0.0f, 0.0f); // PID 튜닝위해 초기값 설정 Kp=2.0
+	HAL_TIM_Base_Start_IT(&htim4);
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1) {
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-	  IMU_ReadData();
-      BME280_Process();
-      IMU_Process();
-  }
-  /* USER CODE END 3 */
+		/* USER CODE BEGIN 3 */
+		IMU_ReadData();
+		BME280_Process();
+		IMU_Process();
+	}
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 168;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 8;
+	RCC_OscInitStruct.PLL.PLLN = 168;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 	if (huart->Instance == USART3) {
 		IMU_RxCallback();
-	}else if (huart->Instance == UART4) {
+	} else if (huart->Instance == UART4) {
 		BLT_ProcessPacket();
 	} else if (huart->Instance == USART1) {
-	      switch (rpi_state) {
-	      	  case 0:
-	      		  if (rpi_rx_buf == 0xAA) {
-	      			  rpi_state = 1;
-	      		  } else if (rpi_rx_buf == 't') {
-	      			  rpi_str_idx = 0;
-	      			  rpi_str_buf[rpi_str_idx++] = 't';
-	      			  rpi_state = 3;
-	      		  }
-			  break;
-	          case 1:
-	              rpi_cmd = rpi_rx_buf;
-	              rpi_state = 2;
-	              break;
-	          case 2:
-	              if (rpi_rx_buf == 0x55) {
-	                  if      (rpi_cmd == 0x0A) BME280_RxCallback();
-	                  else if (rpi_cmd == 0x0B) IMU_RxCallback_RPI();
-	              }
-	              rpi_state = 0;
-	              break;
-	          case 3:
-	        	  if (rpi_rx_buf == '\n') {
-	        		  rpi_str_buf[rpi_str_idx] = '\0';
-	        		  char *str = rpi_str_buf + 1;
-	        		  sscanf(str, "%f", &target_angle);
-	        		  rpi_state = 0;
-	        	  } else {
-	        		  if (rpi_str_idx < sizeof(rpi_str_buf) - 1)
-	        			  rpi_str_buf[rpi_str_idx++] = rpi_rx_buf;
-	        	  }
-				break;
-	      }
-	      HAL_UART_Receive_IT(&huart1, &rpi_rx_buf, 1);
-	  }
+		switch (rpi_state) {
+		case 0:
+			if (rpi_rx_buf == 0xAA) {
+				rpi_state = 1;
+			} else if (rpi_rx_buf == 't') {
+				rpi_str_idx = 0;
+				rpi_str_buf[rpi_str_idx++] = 't';
+				rpi_state = 3;
+			}
+			break;
+		case 1:
+			rpi_cmd = rpi_rx_buf;
+			rpi_state = 2;
+			break;
+		case 2:
+			if (rpi_rx_buf == 0x55) {
+				if (rpi_cmd == 0x0A)
+					BME280_RxCallback();
+				else if (rpi_cmd == 0x0B)
+					IMU_RxCallback_RPI();
+			}
+			rpi_state = 0;
+			break;
+		case 3:
+			if (rpi_rx_buf == '\n') {
+				rpi_str_buf[rpi_str_idx] = '\0';
+				char *str = rpi_str_buf + 1;
+				sscanf(str, "%f,%hhu", &target_angle, &rpi_stop_flag);
+				rpi_state = 0;
+			} else {
+				if (rpi_str_idx < sizeof(rpi_str_buf) - 1)
+					rpi_str_buf[rpi_str_idx++] = rpi_rx_buf;
+			}
+			break;
+		}
+		HAL_UART_Receive_IT(&huart1, &rpi_rx_buf, 1);
+	}
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-    if (htim->Instance == TIM4) {
-    	Process_By_Mode();
-    }
+	if (htim->Instance == TIM4) {
+		Process_By_Mode();
+	}
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
