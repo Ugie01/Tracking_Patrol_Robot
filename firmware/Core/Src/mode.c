99dd98947606c0434f1a9976b4067fbc6f2ea685
김지우 <rlawldn0412@gmail.com>
2026-05-15 20:10:05 +0900

    fix(fw): 트래킹 모드 fall-through 버그 수정 및 함수 참조 교체
    - case MODE_TRACKING에 break 추가 (수동 모드 fall-through 방지)
    - Nav_* 함수를 Motor_* 함수로 교체 (navigation 모듈 제거 대응)
    - ROTATE_SPEED, STRAIGHT_SPEED를 매크로에서 uint8_t 변수로 변경
    - 수동 모드 직진 외 Move_Robot 호출을 blt.c에서 mode.c로 이동

--- firmware/Core/Src/mode.c
+++ firmware/Core/Src/mode.c
@@ -8,16 +8,20 @@
 #include "mode.h"
 #include "motor.h"
 #include "sensor_types.h"
-#include "navigation.h"
+#include "blt.h"
 
 extern IMU_Data imu_data;
 extern uint8_t is_straight_requested;
 extern uint8_t target_speed;
 extern uint8_t target_dir;
 extern uint8_t rpi_stop_flag;
+extern uint8_t bl_buffer[BT_PACKET_SIZE];
 uint8_t current_robot_mode = 1;
 static uint8_t is_first_entry = 1;
 static uint8_t none_cnt = 0;
+uint8_t ROTATE_SPEED = 80;
+uint8_t STRAIGHT_SPEED = 150;
+float target_angle = 0.0f;
 
 void Set_RobotMode(uint8_t mode) {
 //	if (current_robot_mode != mode) {
@@ -39,7 +43,7 @@ void Process_By_Mode(void) {
 		float diff = Get_Diff();
 		if (target_angle == TARGET_ANGLE_NONE_SIGN) {
 			if ( none_cnt >= NONE_TRIGGER ) {
-				Nav_Search();
+				Object_Search();
 				none_cnt = 0;
 			} else {
 				none_cnt ++;
@@ -48,30 +52,33 @@ void Process_By_Mode(void) {
 			none_cnt = 0;
 			if (rpi_stop_flag) {
 				if (diff >= ROTATE_TRIGGER) {
-					Nav_RotateTo(target_angle, imu_data.yaw_f);
+					Rotate_Robot(target_angle, imu_data.yaw_f);
 				}
 				else{
 					Stop_Robot();
 				}
 			} else {
 				if (diff >= ROTATE_TRIGGER) {
-					Nav_RotateTo(target_angle, imu_data.yaw_f);
+					Rotate_Robot(target_angle, imu_data.yaw_f);
 				} else {
 					target_dir = 0;
-					Nav_DriveStraight(ROTATE_SPEED, imu_data.yaw_f);
+					Straight_Robot(STRAIGHT_SPEED, &imu_data.yaw_f, target_dir);
 				}
 			}
 		}
+		break;
 
 	case MODE_MANUAL:
 		if (is_straight_requested) {
 			if (is_first_entry) {
-				Nav_Reset(imu_data.yaw_f);
+				MOTOR_PID_Reset(imu_data.yaw_f);
 				is_first_entry = 0;
 			}
-			Nav_DriveStraight(target_speed, imu_data.yaw_f);
+			Straight_Robot(target_speed, &imu_data.yaw_f, target_dir);
 		} else {
 			is_first_entry = 1;
+			// 직진이 아니면 즉시 모터 제어
+			Move_Robot(bl_buffer[2], bl_buffer[3], bl_buffer[4], bl_buffer[5]);
 		}
 		break;
 	}
